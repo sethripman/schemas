393bd590cb371d9cfee0a7a8e5e23a714106ea60
sethripman <sethripman@gmail.com>
2019-12-09 14:04:40 -0800

    passing non-required tests

--- __tests__/Validator.test.js
+++ __tests__/Validator.test.js
@@ -41,5 +41,41 @@ describe('Validator', () => {
     });
   });
   
+  describe('optional fields', () => {
+    beforeAll(() => {
+      validator = new Validator('age', {
+        type: Number
+      });
+    });
   
+    it('returns the field', () => {
+      const wine = {
+        name: 'vinto',
+        age: 20,
+        price: '1000 buckaroos'
+      };
+  
+      expect(validator.validate(wine)).toEqual(20);
+    });
+  
+    it('casts field to the inputted type', () => {
+      const wine = {
+        name: 'vinto',
+        age: '20',
+        price: '1000 buckaroos'
+      };
+  
+      expect(validator.validate(wine)).toEqual(20);
+    });
+  
+    it('returns null if field is not required', () => {
+      const wine = {
+        name: 'vinto',
+        age: 20,
+        price: '1000 buckaroos'
+      };
+  
+      expect(validator.validate(wine)).toBeNull();
+    });
+  });
 });
