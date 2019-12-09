5770fd72ad0072f2def0c5c2b06045552858068d
sethripman <sethripman@gmail.com>
2019-12-09 14:17:33 -0800

    schema built out, testing

--- __tests__/Schema.test.js
+++ __tests__/Schema.test.js
@@ -21,5 +21,36 @@ describe('Schema tests', () => {
       age: 200,
       price: 4999
     };
+
+    expect(schema.validate(daGoodVino)).toEqual({
+      name: 'The Prost with the Most',
+      age: 200,
+      price: 4999
+    });
+  
+  });
+  
+  it('throws error on object not matching schema', () => {
+    const schema = new Schema({
+      name: {
+        type: String,
+        required: true
+      },
+      age: {
+        type: Number
+      },
+      price: {
+        type: Number,
+        reqired: true
+      }
+    });
+
+    const daMediocreVino = {
+      name: 'delicious rose with pineapple and cheddar flavor',
+      age: 'Beware death by drowning, beware the Ides of March!',
+      price: 12
+    };
+  
+    expect(() => schema.validate(daMediocreVino)).toThrowErrorMatchingSnapshot();
   });
 });
